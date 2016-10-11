f0602212a193eb7aac90809bbe9879afe24f243b
B-raw <michaelbarry1989@hotmail.com>
2016-10-11 15:35:53 +0100

    refactor player attack

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,23 +2,31 @@ require 'player'
 
 describe Player do
 
-  subject { described_class.new("Jen") }
+  subject(:jen) { described_class.new("Jen") }
+  subject(:dave) { described_class.new('Dave') }
 
   describe '#name' do
     it 'returns name' do
-      expect(subject.name).to eq "Jen"
+      expect(jen.name).to eq "Jen"
     end
   end
 
   describe '#health' do
     it 'has initial health 60' do
-      expect(subject.health).to eq 60
+      expect(subject.health).to eq described_class::DEFAULT_HEALTH
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(dave).to receive(:reduce_health)
+      jen.attack(dave)
     end
   end
 
   describe '#reduce_health' do
     it 'will reduce player health points when attacked' do
-      expect(subject.reduce_health).to eq 50
+      expect(dave.reduce_health).to change {dave.health}.by(-10)
     end
   end
 end

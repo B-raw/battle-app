7bdd4af411dac5bf13b54245aebee0a46cac2894
B-raw <michaelbarry1989@hotmail.com>
2016-10-11 15:13:26 +0100

    Add HP capybara + player tests

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -20,4 +20,10 @@ feature "Attacking" do
     click_button 'Attack'
     expect(page).to have_text("Attack successful")
   end
+
+  scenario "Player 1 attacks player 2 and reduces HP by 10" do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_text("Michael HP: 50")
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,4 +9,10 @@ describe Player do
       expect(subject.name).to eq "Jen"
     end
   end
+
+  describe '#health' do
+    it 'has initial health 60' do
+      expect(subject.health).to eq 60
+    end
+  end
 end

b423d1a6905c06a0bd1fa197588be796246ff3a1
B-raw <michaelbarry1989@hotmail.com>
2016-10-11 11:27:33 +0100

    Add form to /

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -4,8 +4,8 @@ feature "Names form" do
   scenario "User can input their name" do
     visit '/'
 
-    fill_in "Player 1 name", :with => "Jennifer"
-    fill_in "Player 2 name", :with => "Michael"
+    fill_in "player_one_name", :with => "Jennifer"
+    fill_in "player_two_name", :with => "Michael"
     click_button "Submit names"
 
     expect(page).to have_text("Players created")

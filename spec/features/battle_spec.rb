3f62bc5845c17c00cca4c8c269f827a401d7461e
hanrattyjen <hanratty.jen@gmail.com>
2016-10-11 12:30:24 +0100

    Added viewing HP to feature spec.

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -11,3 +11,16 @@ feature "Names form" do
     expect(page).to have_text("Jennifer Vs. Michael")
   end
 end
+
+feature "Viewing hit points" do
+  scenario "Player 1 can see player 2's hit points" do
+    visit '/'
+    fill_in "player_1_name", :with => "Jennifer"
+    fill_in "player_2_name", :with => "Michael"
+    click_button "Submit names"
+
+    expect(page).to have_text("Michael HP: ")
+
+
+  end
+end

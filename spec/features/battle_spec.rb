5673e0e0f333772353af32805e7e6758676dc4c0
B-raw <michaelbarry1989@hotmail.com>
2016-10-11 14:12:38 +0100

    Add sign_in_and_play helper class

--- spec/features/battle_spec.rb
+++ spec/features/battle_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 feature "Names form" do
   scenario "User can input their name" do
-    visit '/'
-
-    fill_in "player_1_name", :with => "Jennifer"
-    fill_in "player_2_name", :with => "Michael"
-    click_button "Submit names"
+    sign_in_and_play
 
     expect(page).to have_text("Jennifer Vs. Michael")
   end
@@ -14,10 +10,7 @@ end
 
 feature "Viewing hit points" do
   scenario "Player 1 can see player 2's hit points" do
-    visit '/'
-    fill_in "player_1_name", :with => "Jennifer"
-    fill_in "player_2_name", :with => "Michael"
-    click_button "Submit names"
+    sign_in_and_play
 
     expect(page).to have_text("Michael HP: ")
 
---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,7 @@
+def sign_in_and_play
+  visit '/'
+
+  fill_in "player_1_name", :with => "Jennifer"
+  fill_in "player_2_name", :with => "Michael"
+  click_button "Submit names"
+end

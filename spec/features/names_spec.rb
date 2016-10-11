17ffa7fd35aeb3e8e65584c34d2906762d481dd9
B-raw <michaelbarry1989@hotmail.com>
2016-10-11 12:16:16 +0100

    enable sessions

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -4,8 +4,8 @@ feature "Names form" do
   scenario "User can input their name" do
     visit '/'
 
-    fill_in "player_one_name", :with => "Jennifer"
-    fill_in "player_two_name", :with => "Michael"
+    fill_in "player_1_name", :with => "Jennifer"
+    fill_in "player_2_name", :with => "Michael"
     click_button "Submit names"
 
     expect(page).to have_text("Jennifer Vs. Michael")
